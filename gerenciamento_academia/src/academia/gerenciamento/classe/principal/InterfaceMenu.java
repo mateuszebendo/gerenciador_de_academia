eef1ca6d63a97fb5addcab30e1ec67f012ffa068
Khezac <khezac1@gmail.com>
2024-04-09 17:10:54 -0300

    duplicate fix

--- gerenciamento_academia/src/academia/gerenciamento/classe/principal/InterfaceMenu.java
+++
@@ -1,6 +0,0 @@
-package academia.gerenciamento.classe.principal;
-
-public interface InterfaceMenu {
-	void exibirMenu();
-	void selecionarOpcao();
-}
--- gerenciamento_academia/src/academia/gerenciamento/main/SistemaAcademia.java
+++
@@ -1,9 +0,0 @@
-package academia.gerenciamento.main;
-
-public class SistemaAcademia {
-
-	public static void main(String[] args) {
-		
-	}
-	
-}
