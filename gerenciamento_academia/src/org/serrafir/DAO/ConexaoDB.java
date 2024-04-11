f40089a9d5e91ef992f2bd09773e7b78acebac10
mateuszebendo <mateuszebendovp30@gmail.com>
2024-04-11 17:32:04 -0300

    refactor: alterei conexaoDB

--- gerenciamento_academia/src/org/serrafir/DAO/ConexaoDB.java
+++ gerenciamento_academia/src/org/serrafir/DAO/ConexaoDB.java
@@ -3,6 +3,7 @@ package org.serrafir.DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ConexaoDB {
 	String url = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:5432/postgres?user=postgres.ximiuubjkqrxjuxmjnrh&password=c4iX4zanot1!";
@@ -20,26 +21,11 @@ public class ConexaoDB {
 		}
 	}
 	
-	public void inserirDados () {
-		String sql = "insert into pessoa (nome, cpf, senha) values (?, ?, ?)";
+	public void fecharConexao () {
 		try {
-			PreparedStatement ps = conexao.prepareStatement(sql);
-			
-			ps.setString(1, "Khezac");
-			ps.setString(2, "Heitor");
-			ps.setString(3, "Bianco");
-			
-			ps.executeUpdate();
-			
-		} catch(Exception e) {
-			System.out.println(e);
+			conexao.close();
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e);
 		}
-	}
-	
-	public static void main(String[] args) {
-		ConexaoDB conexao = new ConexaoDB();
-		
-		conexao.criarConexao();
-		conexao.inserirDados();
 	}
 }
